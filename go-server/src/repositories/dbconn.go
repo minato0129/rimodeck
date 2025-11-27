7d1948e0fac4b52fb64963fb7c02de696c9cd911
minato0129 <gororyo0129@gmail.com>
2025-11-27 14:18:13 +0900

    マイグレーションを追記

--- go-server/src/repositories/dbconn.go
+++ go-server/src/repositories/dbconn.go
@@ -28,10 +28,12 @@ func Init() {
 	db.Migrator().DropTable(&models.Slides{})
 	db.Migrator().DropTable(&models.Peages{})
 	db.Migrator().DropTable(&models.Questions{})
+	db.Migrator().DropTable(&models.Tokens{})
 	
 	// マイグレーションを実行
 	db.AutoMigrate(&models.Users{})
 	db.AutoMigrate(&models.Slides{})
 	db.AutoMigrate(&models.Peages{})
 	db.AutoMigrate(&models.Questions{})
+	db.AutoMigrate(&models.Tokens{})
 }
