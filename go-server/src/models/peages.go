14bf79945b73e1364f63049b7858f025c243a5e5
minato0129 <gororyo0129@gmail.com>
2026-01-19 11:21:53 +0900

    cleanup: 未使用のモデル、テスト、バックアップファイルの削除

--- go-server/src/models/peages.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Peages struct {
-	PeageID  string `gorm:"primaryKey"` // peageid
-	PeageNum int  // ページ番号
-	SlideID  string // 外部キー
-	Memo     string
-	Questions []Questions `gorm:"foreignKey:PageID"` // 
-}
--- go-server/src/models/questions.go
+++ go-server/src/models/questions.go
@@ -1,11 +1,10 @@
 package models
 
-import "time"
-
-type Questions struct {
-	QuestionID string `gorm:"primaryKey"` // questionid
-	Txt        string // 質問テキスト
-	PageID     string // peageのIDを参照すると想定
-	CreatedAt  time.Time
-	Peage Peages `gorm:"foreignKey:PageID"`
-}
+//import "time"
+//今は使わない
+// type Questions struct {
+// 	QuestionID string `gorm:"primaryKey"` // questionid
+// 	Txt        string // 質問テキスト
+// 	PageID     string // pageのIDを参照すると想定
+// 	CreatedAt  time.Time
+// }
