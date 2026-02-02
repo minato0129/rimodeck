dbfd543584438dc02cb409eeac114eb9991090a5
minato0129 <gororyo0129@gmail.com>
2026-02-02 10:41:31 +0900

    env読み込み削除

--- go-server/src/init.go
+++ go-server/src/init.go
@@ -1,25 +1,22 @@
 package main
 
 import (
-	"log"
 	"rimodeck/repositories"
-	"github.com/joho/godotenv"
 )
 
-// .envを呼び出します。
-func loadEnv() {
-	// .envファイル全体を読み込み
-	err := godotenv.Load(".env")
+// // .envを呼び出します。
+// func loadEnv() {
+// 	// .envファイル全体を読み込み
+// 	err := godotenv.Load(".env")
 
-	// エラーの場合
-	if err != nil {
-		log.Fatalf("読み込み出来ませんでした: %v", err)
-	}
-}
+// 	// エラーの場合
+// 	if err != nil {
+// 		log.Fatalf("読み込み出来ませんでした: %v", err)
+// 	}
+// }
 
 func Init() {
-	//ENV を読み込み
-	loadEnv()
+
 	//データベース接続、マイグレーション
 	repositories.Init()
 	
